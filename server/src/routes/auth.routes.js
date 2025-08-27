b3403b4965a354b136e37a41f6d505edf90bfbe2
TranMinhThongg <thongcaulong5@gmail.com>
2025-08-27 21:25:19 +0700

    Xong chức năng đăng kí bằng email, gửi OTP xác thực qua mail và gửi lại OTP

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postLogin, getMe, postLogout, postAddAddress, postRegister } from "../controllers/auth.controller.js";
+import { postLogin, getMe, postLogout, postAddAddress, postRegister, postFirebaseLogin, postVerifyOtp, postResendOtp} from "../controllers/auth.controller.js";
 import { requireAuth } from "../middlewares/requireAuth.js";
 const router = Router();
 
@@ -8,4 +8,7 @@ router.get("/me", requireAuth, getMe);
 router.post("/logout", requireAuth, postLogout);
 router.post("/add-address", requireAuth, postAddAddress);
 router.post("/register", postRegister); // body: { email, password }
+router.post("/firebase-login", postFirebaseLogin);
+router.post("/verify-otp", postVerifyOtp); // body: { email, otp, password, name }
+router.post("/resend-otp", postResendOtp);
 export default router;
